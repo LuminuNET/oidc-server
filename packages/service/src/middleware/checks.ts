4cd05ac25959eeb87357b7bdbb81bb1662f757fc
Keimeno <constantin@metzworld.com>
2019-12-18 20:30:11 +0100

    [added] verify oidc queries check

--- packages/service/src/middleware/checks.ts
+++ packages/service/src/middleware/checks.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { HTTP400Error } from '../utils/httpErrors';
+import TService from '../types/ServiceType';
 
-export const checkOidcQueries = (
+let services: Array<TService>;
+
+const loadServices = async () => {
+	if (!services) {
+		services = await import('../config/services.json' as 'json');
+	}
+};
+
+loadServices();
+
+export const getServices = (): Array<TService> => {
+	return services;
+};
+
+export const checkExistsOidcQueries = (
 	req: Request,
 	res: Response,
 	next: NextFunction
@@ -18,4 +33,52 @@ export const checkOidcQueries = (
 	} else {
 		next();
 	}
+};
+
+export const verifyOidcQueries = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	// Check if valid implicit response types are set in response_type query
+	const responseTypes: Array<String> = req.query.response_type.split(' ');
+
+	switch (responseTypes.length) {
+		case 1:
+			if (responseTypes[0] !== 'id_token')
+				throw new HTTP400Error('Missing id token request');
+			break;
+		case 2:
+			if (responseTypes.indexOf('id_token') === -1) {
+				throw new HTTP400Error('Missing id token request');
+			} else if (responseTypes.indexOf('token') === -1) {
+				throw new HTTP400Error('Missing token request');
+			}
+			break;
+		default:
+			throw new HTTP400Error('Too many response types set');
+	}
+
+	// Check if service exists
+	if (!services[req.query.client_id]) {
+		throw new HTTP400Error('Client not found');
+	}
+
+	// Check if the redirect uri is valid
+	if (services[req.query.client_id].callback_url !== req.query.redirect_uri) {
+		throw new HTTP400Error('Redirect Uri invalid');
+	}
+
+	// Check if scope is valid
+	const scopes: Array<String> = req.query.scope.split(' ');
+	if (scopes.indexOf('openid') === -1) {
+		throw new HTTP400Error('OpenID not used');
+	}
+
+	if (scopes.length === 1) {
+		throw new HTTP400Error('No permissions requested');
+	}
+
+	// End validation by calling next function
+	next();
 };
