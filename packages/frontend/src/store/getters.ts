fb2d057ecd2bcbb9c9b8b5d65fefbb534ee548ed
Keimeno <constantin@metzworld.com>
2019-12-28 18:52:03 +0100

    [added] global loading state

--- packages/frontend/src/store/getters.ts
+++ packages/frontend/src/store/getters.ts
@@ -1,4 +1,10 @@
-import { GET_OIDC, GET_VALIDITY } from './getters.type';
+import {
+	GET_OIDC,
+	GET_VALIDITY,
+	GET_PROMPT,
+	GET_LOADERS,
+	GET_FINISHED_LOADERS
+} from './getters.type';
 import { TOidcInput } from '@luminu/types';
 
 const getters = {
@@ -7,6 +13,15 @@ const getters = {
 	},
 	[GET_VALIDITY](store: any): boolean {
 		return store.isValid;
+	},
+	[GET_PROMPT](store: any): string {
+		return store.prompt;
+	},
+	[GET_LOADERS](store: any): number {
+		return store.loaders;
+	},
+	[GET_FINISHED_LOADERS](store: any): number {
+		return store.finishedLoaders;
 	}
 };
 
