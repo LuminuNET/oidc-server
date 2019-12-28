a0d35c07ac1c2a3abcd963580d9a9e3d534d6e2a
Keimeno <constantin@metzworld.com>
2019-12-28 19:10:12 +0100

    [refactored] middleware checks

--- packages/service/src/middleware/checks/authorize/informationChecks.ts
+++ packages/service/src/middleware/checks/authorize/informationChecks.ts
@@ -1,15 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import TService from '../../../types/ServiceType';
-import { getServices } from '../../configuration';
 import { HTTP400Error } from '../../../utils/httpErrors';
-
-let services: Array<TService>;
-
-const loadServices = async () => {
-	services = await getServices();
-};
-
-loadServices();
 
 export const checkExistsInformationQueries = (
 	{ query }: Request,
