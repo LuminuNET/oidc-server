945743ba19a1b609b76d210a0be35659d9de437a
Keimeno <constantin@metzworld.com>
2019-12-28 19:06:33 +0100

    [added] global loading state

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,8 +1,14 @@
 <template>
 	<div id="app">
-		<lm-header :image="require('@/assets/style-logo.png')" />
-		<lm-custom-sticky-header
-			:links="[
+		<div :class="{ loader: loading }">
+			<div class="overlay-loader">
+				<lm-loader class="lm-loader" :mtop="20" :mbottom="20" :size="24" :justifyCenter="true" />
+				<h3>{{ $t('global.buildingWebsite') }}...</h3>
+			</div>
+
+			<lm-header :image="require('@/assets/style-logo.png')" />
+			<lm-custom-sticky-header
+				:links="[
 				{
 					name: 'overview',
 					to: 'hi',
@@ -10,9 +16,10 @@
 					hasChildren: false
 				}
 			]"
-		/>
-		<router-view class="view" />
-		<lm-custom-footer />
+			/>
+			<router-view class="view" />
+			<lm-custom-footer />
+		</div>
 	</div>
 </template>
 
@@ -20,29 +27,77 @@
 import Vue from "vue";
 import { mapActions, mapGetters } from "vuex";
 
-import { LmHeader } from "@luminu/components";
+import { LmHeader, LmLoader } from "@luminu/components";
 import LmCustomStickyHeader from "@/components/layout/CustomStickyHeader.vue";
 import LmCustomFooter from "@/components/layout/CustomFooter.vue";
 
-import { ENTER_OIDC } from "./store/actions.type";
+import { ENTER_OIDC, ENTER_PROMPT } from "./store/actions.type";
 import { GET_OIDC, GET_VALIDITY } from "./store/getters.type";
+import { UPDATE_LOADING_STATE } from "./store/mutations.type";
 
 export default Vue.extend({
 	components: {
 		LmCustomStickyHeader,
 		LmHeader,
-		LmCustomFooter
+		LmCustomFooter,
+		LmLoader
 	},
 	methods: {
-		...mapActions([ENTER_OIDC]),
+		...mapActions([ENTER_OIDC, ENTER_PROMPT]),
 		...mapGetters([GET_OIDC, GET_VALIDITY])
 	},
 	created() {
 		this[ENTER_OIDC]();
-	}
+		this[ENTER_PROMPT]();
+
+		this.$store.subscribe((mutation, state) => {
+			if (mutation.type === UPDATE_LOADING_STATE) {
+				this.loading = state.loading;
+			}
+		});
+	},
+	data: () => ({
+		loading: true
+	})
 });
 </script>
 
 <style lang="scss">
 @import "~@luminu/core/scss/_globals.scss";
+
+.overlay-loader {
+	display: none;
+
+	* {
+		display: none;
+	}
+}
+
+.loader {
+	* {
+		display: none;
+	}
+
+	.overlay-loader {
+		display: unset;
+		height: 100vh;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		z-index: 1000000;
+		background: $background;
+		flex-direction: column;
+
+		h3 {
+			font-weight: 400;
+			color: #555;
+		}
+
+		* {
+			display: unset;
+		}
+	}
+}
 </style>
