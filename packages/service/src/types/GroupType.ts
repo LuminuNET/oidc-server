cb5a08d11e8fb6ece35263019f6fff8e12816f3c
Keimeno <constantin@metzworld.com>
2019-12-30 14:49:08 +0100

    [added] GroupType

---
+++ packages/service/src/types/GroupType.ts
@@ -0,0 +1,11 @@
+type TGroup = {
+	display_style_priority: number;
+	user_group_id: number;
+	title: string;
+	username_css: string;
+	user_title: string;
+	banner_css_class: string;
+	banner_text: string;
+};
+
+export default TGroup;
